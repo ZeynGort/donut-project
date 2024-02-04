75139dd2b677cd5afab6e93dccece96dc12e6a15
ZeynGort <zeyngort323@gmail.com>
2024-02-04 23:29:16 +0200

    pull dev

--- src/js/menu.js
+++ src/js/menu.js
@@ -10,17 +10,18 @@ refs.toggleMenuBtn.addEventListener('click', toggleMenu);
 refs.links.forEach(element => element.addEventListener('click', toggleMenu));
 
 function toggleMenu() {
-  const isOpened = refs.svgOpen.getAttribute('open');
-
-  if (isOpened === 'false') {
-    refs.menu.classList.add('open');
-    document.querySelector('body').classList.add('noscroll');
-    refs.svgOpen.setAttribute('open', 'true');
-    refs.svgClose.setAttribute('close', 'false');
-  } else {
-    refs.menu.classList.remove('open');
-    document.querySelector('body').classList.remove('noscroll');
-    refs.svgOpen.setAttribute('open', 'false');
-    refs.svgClose.setAttribute('close', 'true');
+  if (window.innerWidth < 1280) {
+    const isOpened = refs.svgOpen.getAttribute('open');
+    if (isOpened === 'false') {
+      refs.menu.classList.add('open');
+      document.querySelector('body').classList.add('noscroll');
+      refs.svgOpen.setAttribute('open', 'true');
+      refs.svgClose.setAttribute('close', 'false');
+    } else {
+      refs.menu.classList.remove('open');
+      document.querySelector('body').classList.remove('noscroll');
+      refs.svgOpen.setAttribute('open', 'false');
+      refs.svgClose.setAttribute('close', 'true');
+    }
   }
 }
